3099e5966aaabf8f55820f600eb71df1542595d6
Harrison Parkes <hpa101@uclive.ac.nz>
2025-04-08 12:46:54 +1200

    Chore: Removed unessary appending of readme to rustdoc of main.rs

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,6 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
-#![doc = include_str!("../README.md")]
-
 mod network;
 
 use std::{error::Error, io::Write, path::PathBuf};
